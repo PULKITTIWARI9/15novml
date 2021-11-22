dc1843d62ba6f0d5baf90d8694be415b9d7254ec
PULKITTIWARI9 <94830287+PULKITTIWARI9@users.noreply.github.com>
2021-11-22 14:29:18 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,16 @@
+S='Hello this is python'
+S.split('i')
+a='this is nice'
+b=88
+c=77.9
+d=True
+f=False
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(c*b)
+ print('the sum of b and c is ',b+c)
+ print('d and f that is bool1 and bool2 gives',d and f)
+ 
+
